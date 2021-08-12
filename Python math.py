104e6215baf82610e1ea1c66e76ff1c6775e2da4
Leonardo <leo.araujogomes@hotmail.com>
2021-08-12 18:43:16 -0300

    python math

---
+++ Python math.py
@@ -0,0 +1,12 @@
+# visualizar equivalências de ângulos em graus
+import math
+
+a = math.radians(30)
+b = math.radians(45)
+c = math.radians(60)
+
+print("30º são ", a, "radianos")
+print("45º são ", b, "radianos")
+print("60º são ", c, "radianos")
+print("90º são ", math.radians(90), "radianos")
+print("180º são ", math.radians(180), "radianos")
